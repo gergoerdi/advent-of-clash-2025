354f6b7a3c3efa4b808bebc937097022175fd007
Gergo ERDI <gergo@erdi.hu>
2025-12-21 17:17:47 +0800

    Generalize by number of digits to choose

--- src/AOC2025/P03/Main.hs
+++ src/AOC2025/P03/Main.hs
@@ -1,12 +1,16 @@
 {-# LANGUAGE ApplicativeDo, RecordWildCards, BlockArguments #-}
+{-# LANGUAGE ScopedTypeVariables, TypeApplications #-}
 module Main where
 
 import Options.Applicative
 import Data.Traversable
 import Data.Char (digitToInt)
 
+import Debug.Trace
+
 data Options = Options
     { inFile :: FilePath
+    , batteries :: Int
     }
 
 opts :: Parser Options
@@ -16,23 +20,37 @@ opts = do
       , short 'i'
       , help "input filename"
       ]
+
+    batteries <- option auto . mconcat $
+      [ long "batteries"
+      , short 'n'
+      , help "Number of batteries to choose"
+      , value 2
+      ]
+
     pure Options{..}
 
-type Digit = Int
-
-solve :: (Ord a, Bounded a, Traversable t, Applicative t) => t a -> (a, a)
-solve xs = (x, y)
+solve :: forall a t. (Show a, Ord a, Bounded a, Traversable t, Applicative t, t ~ []) => Int -> t a -> t a
+solve n xs = r
   where
-    (_, rs) = mapAccumR (\acc x -> let acc' = acc `max` x in (acc', acc')) minBound xs
-    (_, ls) = mapAccumL (\acc x -> let acc' = acc `max` x in (acc', acc)) minBound xs
-    (x, y) = maximum $ (,) <$> ls <*> rs
+    f acc x = let acc' = maybe id max acc x in (Just acc', acc')
+
+    go :: Int -> (t a, t [a], [a])
+    go 1 = let (Just r, ys) = mapAccumR f Nothing $ map (:[]) xs
+           in (xs, ys, r)
+    go n = let (xs', ys, _) = go (n - 1)
+               xs'' = minBound : xs'
+               (Just r, ys') = mapAccumR f Nothing $ zipWith (:) xs'' ys
+           in (xs'', ys', r)
+
+    (_, _, r) = go n
 
 main :: IO ()
 main = do
     Options{..} <- execParser $ info (opts <**> helper) fullDesc
     problems <- lines <$> readFile inFile
     s <- sum <$> for problems \prob -> do
-        let (x, y) = solve (ZipList . map digitToInt $ prob)
-            val = (x * 10 + y)
+        let result = solve batteries (map digitToInt prob)
+            val = foldl (\s x -> 10 * s + x) 0 result
         pure val
     print s
