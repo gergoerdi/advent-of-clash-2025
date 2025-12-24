eb2ff549f557909e0f3df7c7f5dce0b0588be8b2
Gergo ERDI <gergo@erdi.hu>
2025-12-24 19:55:02 +0800

    Use BCD representation in `main`, to test that part as well

--- src/AoC2025/P03/Main.hs
+++ src/AoC2025/P03/Main.hs
@@ -5,20 +5,20 @@ module Main where
 import Clash.Prelude
 
 import AoC2025.P03.Solve
+import AoC2025.P03.BCD
 
 import Options.Applicative
-import Data.Traversable
 import qualified Data.List as L
 import Data.Char (digitToInt, intToDigit)
 import Text.Printf
-import Control.Monad (when)
+import Control.Monad (when, foldM)
 
 data Part = Part1 | Part2
 
 data Options = Options
     { inFile :: FilePath
     , part :: Part
-    , debug :: Bool
+    , verbose :: Bool
     }
 
 opts :: Parser Options
@@ -38,28 +38,36 @@ opts = do
         ]
       ]
 
-    debug <- switch . mconcat $
-      [ long "debug"
+    verbose <- switch . mconcat $
+      [ long "verbose"
       ]
 
     pure Options{..}
 
-fromInput :: [a] -> Vec 100 a
+type LineLen = 100
+
+fromInput :: [a] -> Vec LineLen a
 fromInput = L.foldr (\x xs -> fst $ shiftInAt0 xs (x :> Nil)) (pure undefined)
 
-toOutput :: (Num a) => Vec k a -> a
-toOutput = foldl (\s x -> 10 * s + x) 0
+fromBCD :: (KnownNat n) => BCD n -> Natural
+fromBCD = foldl (\s d -> 10 * s + fromIntegral d) 0
 
 main :: IO ()
 main = do
     Options{..} <- execParser $ info (opts <**> helper) fullDesc
-    let solve' = case part of
-            Part1 -> toOutput . solve 2
-            Part2 -> toOutput . solve 12
+
+    let solveLine :: BCD 20 -> String -> IO (BCD 20)
+        solveLine = case part of
+            Part1 -> f (solve 2)
+            Part2 -> f (solve 12)
+          where
+            f :: forall k k0. (KnownNat k, KnownNat k0, 20 ~ k0 + k) => (BCD LineLen -> BCD k) -> BCD 20 -> String -> IO (BCD 20)
+            f solve' s prob = do
+                let result = solve' $ fromInput . fmap (fromIntegral . digitToInt) $ prob
+                when verbose $ printf "%s %d\n" prob (fromBCD result)
+                pure $ snd $ addBCD s result
 
     problems <- lines <$> readFile inFile
-    s <- sum <$> for problems \prob -> do
-        let result = solve' (fromInput $ fmap digitToInt prob)
-        when debug $ printf "%s %d\n" prob result
-        pure result
-    print s
+    s <- foldM solveLine (repeat 0) problems
+
+    print $ fromBCD s
